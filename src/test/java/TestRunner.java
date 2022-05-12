ef0953766b24d564eacab05ba1d5a082ffa3c21a
Rotondwa Netshimbupfe <59872053+TondyNetsh@users.noreply.github.com>
2022-05-12 11:54:16 +0200

    MaxNumberTable first commit

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,7 +1,7 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/feature/Facebook.feature",
-        glue = "stepDefinitions", plugin = {"pretty", "html:target/Report.html"})
+@CucumberOptions(features = "src/test/java/features",
+        glue = "stepdefinitions", plugin = {"pretty", "html:target/Report.html"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
--- src/test/java/stepdefinitions/StepDefinitions.java
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -1,22 +1,30 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDefinitions {
-
-    @Given("^User opens the supersport website$")
+    public WebDriver driver;
+    @Given("User opens the supersport website")
     public void user_opens_the_supersport_website() throws Throwable {
-
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\CMDQ1\\IdeaProjects\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://supersport.com/");
     }
 
     @Then("^User clicks on the DStv Premiership link$")
     public void user_clicks_on_the_dstv_premiership_link() throws Throwable {
-
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div[1]/div[1]/div/ul/li[2]/a/div/div")).click();
     }
 
     @Then("^User clicks on the tables link to show the table$")
     public void user_clicks_on_the_tables_link_to_show_the_table() throws Throwable {
-
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div/ul/li[5]/a")).click();
     }
 
     @Then("^User gets the maximum number of goals scored$")
