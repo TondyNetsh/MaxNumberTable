33a6d40e3b6b538739f3557cce74e22ca35bd940
Rotondwa Netshimbupfe <59872053+TondyNetsh@users.noreply.github.com>
2022-05-12 11:00:01 +0200

    MaxNumberTable first commit

---
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -0,0 +1,31 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+
+public class StepDefinitions {
+
+    @Given("^User opens the supersport website$")
+    public void user_opens_the_supersport_website() throws Throwable {
+
+    }
+
+    @Then("^User clicks on the DStv Premiership link$")
+    public void user_clicks_on_the_dstv_premiership_link() throws Throwable {
+
+    }
+
+    @Then("^User clicks on the tables link to show the table$")
+    public void user_clicks_on_the_tables_link_to_show_the_table() throws Throwable {
+
+    }
+
+    @Then("^User gets the maximum number of goals scored$")
+    public void user_gets_the_maximum_number_of_goals_scored() throws Throwable {
+
+    }
+
+    @And("^User displays the maximum number$")
+    public void user_displays_the_maximum_number() throws Throwable {
+
+    }
+}
